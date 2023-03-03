6e5555fcacf82fc43a11c200659333d9523f9d25
Dragana Spasojevic <draganaspasojevic@Draganas-Air.fritz.box>
2023-03-03 10:56:36 +0100

    some changes, I put files in the right directory

--- practice.sh
+++ practice.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #move to the Challenge directory
+cd Desktop
+cd Exercise01
+cd exercise01
+mkdir Challenge
 cd Challenge
 #create 25 files
 touch dragana{1..25}.txt
--- practice1.sh
+++ practice1.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-if [ -e "$(ls -v | tail -1)" ]; then
-number=$(($(ls -v | tail -1 | sed 's/[^0-9]*//g')+1))
-fi
+cd Users
+cd Desktop
+cd Exercise01
+cd exercise01
+cd Challenge
 
-for i in {1..25}
-do
-        touch dragana$number.txt
-        number=$(($number + 1))
+lastnumber=`find ./ -type f -name "dragana*" | wc -l`
+#add +1 to create the next higher file
+lastnumber=$((lastnumber+1))
+for ((i=1; i<=25; i++)); do
+touch dragana$lastnumber.txt
+lastnumber=$((lastnumber+1))
 done
+ls -l > filenames.txt
